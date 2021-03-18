a5d8d2b7cf41d5f5c0f9227bb8ad6bc4e6ca728f
Fabian Pahl <fabian@21st.digital>
2021-03-18 11:08:37 +0100

    Updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['standard', 'standard-jsx', 'standard-react', 'plugin:prettier/recommended', 'prettier/react'],
+  extends: ['standard', 'standard-jsx', 'standard-react', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
+    // TODO:
+    // prefer-template
+    // import/extensions (js,jsx: nein, rest ja)
+    // no-console: never
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-prop-types.md
+    // react/prop-types: error
+    // object-shorthand: always
+    // react/destructuring-assignment: always
     'prettier/prettier': [
       'error',
       {
@@ -10,8 +18,7 @@ module.exports = {
         printWidth: 120,
         semi: false,
         singleQuote: true,
-        trailingComma: 'none'
-      }
-    ]
-  }
+      },
+    ],
+  },
 }
