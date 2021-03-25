c55ac09cce8222f29c423f45a96b0dfc28d5d39c
Erik Simon <erik@21st.digital>
2021-03-25 11:02:59 +0100

    added more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,27 @@ module.exports = {
   ],
   plugins: ['prettier'],
   rules: {
-    // TODO:
-    // prefer-template
-    // import/extensions (js,jsx: nein, rest ja)
-    // no-console: never
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-prop-types.md
-    // react/prop-types: error
-    // object-shorthand: always
-    // react/destructuring-assignment: always
+    'prefer-template': 'error',
+    // only a warning since it's not auto-fixable and not a major problem
+    'import/extensions': [
+      'warn',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    /* 
+      not auto-fixable yet:
+      'react/sort-prop-types': ['error', { callbacksLast: true, shorthandFirst: true, reservedFirst: true }],
+    */
+    'react/jsx-sort-props': ['error', { callbacksLast: true, shorthandFirst: true, reservedFirst: true }],
+    // skipUndeclared: optional boolean to only error on components that have a propTypes block declared.
+    // should be true for now, maybe remove later
+    'react/prop-types': ['error', { skipUndeclared: true }],
+    'object-shorthand': ['error'],
+    'react/destructuring-assignment': ['warn', 'always'],
     'prettier/prettier': [
       'error',
       {
